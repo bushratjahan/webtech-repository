c25f8d59ec6de540cde4564c264c2887fa019511
bushratjahan <62569445+bushratjahan@users.noreply.github.com>
2021-08-08 20:57:39 +0600

    Add files via upload

---
+++ LabTask7/templates/base2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LabTask</title>
+    <link rel="stylesheet" type="text/css" href="css/footer_base.css">
+    <link rel="stylesheet" type="text/css" href="css/admin_login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+    <main>
+
+    </main>
+    <div class="footer_base">
+      <p>LabTask7</p>
+    </div>
+</body>
+</html>
